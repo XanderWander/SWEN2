5476b6e5e6d3c5b561400948e05975b4b161c497
Quinn <ql.vanderschaar@student.avans.nl>
2020-12-01 23:45:07 +0100

    first working version

---
+++ Feature_3/MergeSort.cpp
@@ -0,0 +1,66 @@
+#include "MergeSort.h"
+#include <iostream>
+using namespace std;
+
+MergeSort::MergeSort(int array[], int num)
+{
+ 
+    merge_sort(array, 0, num - 1);
+    cout << "Sorted array\n";
+    for (int i = 0; i < num; i++)
+    {
+        cout << array[i] << "\t";
+    }
+}
+
+MergeSort::~MergeSort()
+{
+}
+
+
+void MergeSort::merge_sort(int* arr, int low, int high)
+{
+    int mid;
+    if (low < high) {
+        //divide the array at mid and sort independently using merge sort
+        mid = (low + high) / 2;
+        merge_sort(arr, low, mid);
+        merge_sort(arr, mid + 1, high);
+        //merge or conquer sorted arrays
+        merge(arr, low, high, mid);
+    }
+}
+// Merge sort 
+void MergeSort::merge(int* arr, int low, int high, int mid)
+{
+    int i, j, k, c[50];
+    i = low;
+    k = low;
+    j = mid + 1;
+    while (i <= mid && j <= high) {
+        if (arr[i] < arr[j]) {
+            c[k] = arr[i];
+            k++;
+            i++;
+        }
+        else {
+            c[k] = arr[j];
+            k++;
+            j++;
+        }
+    }
+    while (i <= mid) {
+        c[k] = arr[i];
+        k++;
+        i++;
+    }
+    while (j <= high) {
+        c[k] = arr[j];
+        k++;
+        j++;
+    }
+    for (i = low; i < k; i++) {
+        arr[i] = c[i];
+    }
+}
+// read input array and call mergesort
--- Feature_3/main.cpp
+++ Feature_3/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "MergeSort.h"
+using namespace std;
 
 int main() {
-
+    int myarray[30],num;
+    cout << "Enter number of elements to be sorted:";
+    cin >> num;
+    cout << "Enter " << num << " elements to be sorted:";
+    for (int i = 0; i < num; i++) {
+        cin >> myarray[i];
+    }
+	MergeSort sort(myarray,num);
 	return 0; 
 }
