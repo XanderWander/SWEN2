a15d09c18fe6d4816957852147028d17d60c5242
julian <jybruin@avans.nl>
2020-12-01 14:49:03 +0100

    Werkend Algoritme

--- BubbleSortAlgorithem/BubblesortAlgorithem.cpp
+++ BubbleSortAlgorithem/BubblesortAlgorithem.cpp
@@ -4,6 +4,33 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	int a[10] = { 5,4,1,6,3,5,8,4,2,3 };
+	int count;
+
+	while (1)
+	{
+		count = 0;
+		for ( int i = 0; i < 9; i++)
+		{
+			if ( a[i] > a[i + 1] )
+			{
+				int temp = a[i];
+				a[i] = a[i + 1];
+				a[i + 1] = temp;
+				count++;
+			}
+		}
+		if ( count == 0 )
+		{
+			break;
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << a[i] << endl;
+	}
+	cout << "end" << endl;
+
 	return 0;
 }
