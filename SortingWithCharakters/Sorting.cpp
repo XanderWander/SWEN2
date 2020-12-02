b3148ab8306175c56cd2c21b5ccd18e369075e24
julian <jybruin@avans.nl>
2020-12-02 20:36:25 +0100

    Feature 1 en 2 met char

---
+++ SortingWithCharakters/Sorting.cpp
@@ -0,0 +1,152 @@
+#include "Sorting.h"
+
+#include <string>
+#include <iostream>
+#include <sstream>
+
+Sorting::Sorting()
+{
+	amount = 0;
+	status = "";
+}
+
+Sorting::~Sorting()
+{
+
+}
+
+void Sorting::selectArray()
+{
+	int returnValue = 0;
+
+	cout << "Do you want to sort letters or numbers?\n Type c for Charakters and n for numbers : ";
+	getline(cin, status);
+
+}
+
+void Sorting::getArray()
+{
+	string str;
+
+	selectArray();
+
+	if ( status == "n" )
+	{
+		cout << "insert the Amount of numbers\n";
+	}
+	if ( status == "c")
+	{
+		cout << "insert the Amount of charakters\n";
+	}
+
+	getline(std::cin, str);
+	amount = stoi(str);
+
+	for (int i = 0; i < amount; i++) 
+	{
+		if (status == "n")
+		{
+			cout << "number " << i+1 << ":";
+			getline(cin, str);
+			numberArray[i] = stoi(str);
+		}
+
+		if (status == "c")
+		{
+			cout << "charakter " << i << ":";
+			getline(cin, str);
+			string strConverter(str);
+			charArray[i] = strConverter[0];
+		}
+	}
+
+	if (status == "n")
+	{
+		printArray(1);
+	}
+	if (status == "c")
+	{
+		printArray(2);
+	}
+
+}
+
+void Sorting::printArray( int print )
+{
+	for (int i = 0; i < amount; i++)
+	{
+		if ( print == 1 )
+		{
+			cout << numberArray[i] << endl;
+		}
+		if ( print == 2 )
+		{
+			cout << charArray[i] << endl;
+		}
+	}
+}
+
+void Sorting::selectSort()
+{
+	string sortType;
+
+	cout << "How do you want to sort the array's\n For the Bubblesort algorithm type b for the merge and heap method type m :\n";
+	getline(cin, sortType);
+
+	if ( sortType == "b" )
+	{
+		bubbleSort();
+	}
+	if ( sortType == "m" )
+	{
+
+	}
+}
+
+void Sorting::bubbleSort()
+{
+	int count;
+
+	while (1)
+	{
+		count = 0;
+		for (int i = 0; i < amount-1; i++)
+		{
+			if (status == "c" )
+			{
+				if (charArray[i] > charArray[i + 1])
+				{
+					char temp = charArray[i];
+					charArray[i] = charArray[i + 1];
+					charArray[i + 1] = temp;
+					count++;
+				}
+			}
+
+			if (status == "n")
+			{
+				if (numberArray[i] > numberArray[i + 1])
+				{
+					int temp = numberArray[i];
+					numberArray[i] = numberArray[i + 1];
+					numberArray[i + 1] = temp;
+					count++;
+				}
+			}
+			
+		}
+		if (count == 0)
+		{
+			break;
+		}
+	}
+
+	if (status == "n")
+	{
+		printArray( 1 );
+	}
+	if (status == "c")
+	{
+		printArray( 2 );
+	}
+}
---
+++ SortingWithCharakters/Sorting.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Sorting
+{
+private:
+	int numberArray[100] = {};
+	char charArray[100] = {};
+	int amount;
+	string status;
+
+public:
+	Sorting();
+	virtual ~Sorting();
+
+	
+
+public:
+	void selectArray();
+	void getArray();
+	void printArray( int print );
+	void selectSort();
+	void bubbleSort();
+	
+
+
+};
+
