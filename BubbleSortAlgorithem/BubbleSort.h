82b8f890359a5ac8ba562c9d8af772daf307e189
julian <jybruin@avans.nl>
2020-12-02 21:46:04 +0100

    Sort met class

---
+++ BubbleSortAlgorithem/BubbleSort.h
@@ -0,0 +1,14 @@
+#pragma once
+class BubbleSort
+{
+public:
+	int numberArray[5] = { 3,8,3,6,1 };
+
+public:
+	BubbleSort();
+	virtual ~BubbleSort();
+
+	void sorting();
+
+};
+
---
+++ BubbleSortAlgorithem/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "BubbleSort.h"
+
+using namespace std;
+
+int main()
+{
+	BubbleSort sort;
+	sort.sorting();
+	
+	return 0;
+}
